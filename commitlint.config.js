366728873fba6e824ec2b77c856e774e425ca49b
Ivo Cazemier <git@warmemelk.nl>
2026-02-11 22:17:11 +0100

    chore: configure public package access and update commitlint config
    - Added publishConfig.access: public for scoped package
    - Migrated from .commitlintrc.json to commitlint.config.js to support ignores function
    - Disabled body-max-line-length rule for release commits
    - Added .env.local to gitignore

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-case': [2, 'never', ['upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [0],
+  },
+  ignores: [(message) => message.includes('[skip ci]')],
+};
